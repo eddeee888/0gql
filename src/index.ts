9bd29083c3f6625b76d356620e844985effbb171
Eddy Nguyen <ch@eddeee888.me>
2022-09-15 19:40:34 +1000

    Parse importd

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,66 @@
-const thingo = "Test";
-console.log(thingo);
+import ts from "typescript";
+
+const file = "src/tests/test.ts";
+
+const program = ts.createProgram([file], { allowJs: true });
+const sourceFile = program.getSourceFile(file);
+
+if (!sourceFile) {
+  throw new Error("Unable to get source file");
+}
+
+const trimQuotes = (text: string): string => text.replace(/['"]+/g, "");
+
+/**
+ * Function to parse an ImportDeclaration node:
+ *   - returns null if not targeted gql tag module
+ *   - OR returns an array of possible identifiers that might be used as gql tag
+ */
+const parseGqlTagImportIdentifiers = (
+  node: ts.ImportDeclaration,
+  source: ts.SourceFile
+): string[] | null => {
+  const module = trimQuotes(node.moduleSpecifier.getText(source));
+
+  // TODO: there are other modules to check. Maybe let users pass it in?
+  if (module !== "graphql-tag") {
+    return null;
+  }
+
+  const identifiers: string[] = [];
+
+  const defaultImportIdentifier = node.importClause?.name?.getText(source);
+  if (defaultImportIdentifier) {
+    identifiers.push(defaultImportIdentifier);
+  }
+
+  node.importClause?.namedBindings?.forEachChild((cn) => {
+    if (ts.isImportSpecifier(cn)) {
+      if (
+        (!cn.propertyName && cn.name.getText(source) === "gql") ||
+        cn.propertyName?.getText(source) === "gql"
+      ) {
+        identifiers.push(cn.name.getText(source));
+      }
+    }
+  });
+
+  return identifiers;
+};
+
+let gqlTagIdentifiers: string[] = [];
+
+ts.forEachChild(sourceFile, (node: ts.Node) => {
+  console.log("*** entering node:", ts.SyntaxKind[node.kind]);
+  // Check imports for gqlTagIdentifiers
+  if (ts.isImportDeclaration(node)) {
+    const identifiers = parseGqlTagImportIdentifiers(node, sourceFile);
+    if (identifiers) {
+      gqlTagIdentifiers = [...gqlTagIdentifiers, ...identifiers];
+    }
+  }
+
+  if (ts.isVariableStatement(node)) {
+    console.log("=> Variable:", node);
+  }
+});
---
+++ src/tests/test.ts
@@ -0,0 +1,21 @@
+import gql3, { gql as ttt, gql } from "graphql-tag";
+import React from "react";
+import { gql as gql2 } from "graphql-tag";
+
+export const USER_DOCUMENT = gql`
+  query User {
+    user(id: "420") {
+      id
+      name
+    }
+  }
+`;
+
+export const UPDATE_USER_DOCUMENT = gql`
+  mutation UpdateUser {
+    userUpdate(id: "420") {
+      id
+      name
+    }
+  }
+`;
