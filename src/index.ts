cb43f1de391d106bc40587fcdfad8fcf5ac1da24
Eddy Nguyen <ch@eddeee888.me>
2022-09-15 20:18:46 +1000

    Print out to target

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,9 @@
+import { writeFile } from "fs/promises";
+import path from "path";
 import ts from "typescript";
 
-const file = "src/tests/test.ts";
-
-const program = ts.createProgram([file], { allowJs: true });
-const sourceFile = program.getSourceFile(file);
-
-if (!sourceFile) {
-  throw new Error("Unable to get source file");
-}
-
 const trimQuotes = (text: string): string => text.replace(/['"]+/g, "");
+const trimBackTicks = (text: string): string => text.replace(/[`]+/g, "");
 
 /**
  * Function to parse an ImportDeclaration node:
@@ -19,7 +13,7 @@ const trimQuotes = (text: string): string => text.replace(/['"]+/g, "");
 const parseGqlTagImportIdentifiers = (
   node: ts.ImportDeclaration,
   source: ts.SourceFile
-): string[] | null => {
+): Record<string, true> | null => {
   const module = trimQuotes(node.moduleSpecifier.getText(source));
 
   // TODO: there are other modules to check. Maybe let users pass it in?
@@ -27,11 +21,11 @@ const parseGqlTagImportIdentifiers = (
     return null;
   }
 
-  const identifiers: string[] = [];
+  const identifiers: Record<string, true> = {};
 
   const defaultImportIdentifier = node.importClause?.name?.getText(source);
   if (defaultImportIdentifier) {
-    identifiers.push(defaultImportIdentifier);
+    identifiers[defaultImportIdentifier] = true;
   }
 
   node.importClause?.namedBindings?.forEachChild((cn) => {
@@ -40,7 +34,7 @@ const parseGqlTagImportIdentifiers = (
         (!cn.propertyName && cn.name.getText(source) === "gql") ||
         cn.propertyName?.getText(source) === "gql"
       ) {
-        identifiers.push(cn.name.getText(source));
+        identifiers[cn.name.getText(source)] = true;
       }
     }
   });
@@ -48,19 +42,61 @@ const parseGqlTagImportIdentifiers = (
   return identifiers;
 };
 
-let gqlTagIdentifiers: string[] = [];
+function changeExtension(file: string, extension: string) {
+  const basename = path.basename(file, path.extname(file));
+  return path.join(path.dirname(file), basename + extension);
+}
 
-ts.forEachChild(sourceFile, (node: ts.Node) => {
-  console.log("*** entering node:", ts.SyntaxKind[node.kind]);
-  // Check imports for gqlTagIdentifiers
-  if (ts.isImportDeclaration(node)) {
-    const identifiers = parseGqlTagImportIdentifiers(node, sourceFile);
-    if (identifiers) {
-      gqlTagIdentifiers = [...gqlTagIdentifiers, ...identifiers];
+const files = ["./src/tests/simple/test.ts"];
+const targetFiles: { filename: string; content: string }[] = [];
+const program = ts.createProgram(files, { allowJs: false });
+
+files.forEach((file) => {
+  const sourceFile = program.getSourceFile(file);
+
+  if (!sourceFile) {
+    throw new Error("Unable to get source file");
+  }
+
+  const targetFilename = changeExtension(file, ".graphql");
+  let gqlTagIdentifiers: Record<string, true> = {};
+  const graphqlTemplates: string[] = [];
+
+  ts.forEachChild(sourceFile, (node: ts.Node) => {
+    console.log("*** entering node:", ts.SyntaxKind[node.kind]);
+    // Check imports for gqlTagIdentifiers
+    if (ts.isImportDeclaration(node)) {
+      const identifiers = parseGqlTagImportIdentifiers(node, sourceFile);
+      if (identifiers) {
+        gqlTagIdentifiers = { ...gqlTagIdentifiers, ...identifiers };
+      }
     }
-  }
 
-  if (ts.isVariableStatement(node)) {
-    console.log("=> Variable:", node);
-  }
+    if (ts.isVariableStatement(node)) {
+      node.declarationList.declarations.forEach((declaration) => {
+        if (
+          declaration.initializer &&
+          ts.isTaggedTemplateExpression(declaration.initializer)
+        ) {
+          const tagIdentifier = declaration.initializer.tag.getText(sourceFile);
+          if (!gqlTagIdentifiers[tagIdentifier]) {
+            return;
+          }
+          const tagTemplate = trimBackTicks(
+            declaration.initializer.template.getText(sourceFile)
+          );
+          graphqlTemplates.push(tagTemplate);
+        }
+      });
+    }
+  });
+  targetFiles.push({
+    filename: targetFilename,
+    content: graphqlTemplates.join(),
+  });
 });
+
+(async () =>
+  await Promise.all(
+    targetFiles.map((file) => writeFile(file.filename, file.content))
+  ))();
