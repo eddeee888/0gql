3d3549a9c7ab46103408c5c7061b7173bd7ffc93
Eddy Nguyen <ch@eddeee888.me>
2022-09-21 00:24:30 +1000

    Update basic tests

--- src/tests/ts/test.ts
+++ src/tests/ts/test.ts
@@ -2,7 +2,7 @@ import gql3, { gql as ttt, gql } from "graphql-tag";
 import React, { createElement } from "react";
 import { gql as gql2 } from "graphql-tag";
 
-export const USER_DOCUMENT = gql`
+export const TEST1 = gql3`
   query User {
     user(id: "420") {
       id
@@ -11,11 +11,27 @@ export const USER_DOCUMENT = gql`
   }
 `;
 
-export const UPDATE_USER_DOCUMENT = gql`
+export const TEST2 = ttt`
   mutation UpdateUser {
     userUpdate(id: "420") {
       id
       name
+    }
+  }
+`;
+
+export const TEST3 = gql`
+  query User {
+    user(id: "500") {
+      id
+    }
+  }
+`;
+
+export const TEST4 = gql2`
+  query User {
+    user(id: "501") {
+      id
     }
   }
 `;
